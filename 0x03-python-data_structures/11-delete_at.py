33cdd7f84a8b1569c58225fd30c0d6eff6a80c78
UnlockMirara <mnjihia66@gmail.com>
2023-10-07 01:20:51 -0700

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+# 11-delete_at.py
+
+
 
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del(my_list[idx])
-    return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return my_list  # If idx is out of range or negative, return the same list
+
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+
+    return new_list
+
+# Test the function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
